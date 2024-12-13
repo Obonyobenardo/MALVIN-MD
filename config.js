f237512e4f694452427296fe86a9d716d624ba3b
Obonyobenardo <151940477+Obonyobenardo@users.noreply.github.com>
2024-12-14 00:04:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 ///=================When editing, do not accidentally delete anything !!MAIN-CONFIG=================================
-SESSION_ID: process.env.SESSION_ID || "SB1yXLrQ#JJRCLRItIqp35aK76ITLOO51M7Hc4ObrLUFSCq4s6SM",  // ADD Your Session Id 
+SESSION_ID: process.env.SESSION_ID || "bcVTCYbD#C0Piv0R3ZL8eq-l5YkIzlN9VRmJLrgXZRdAL-QygWns",  // ADD Your Session Id 
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "false", //add true or false
@@ -17,8 +17,8 @@ AUTO_STICKER: process.env.AUTO_STICKER || "false", //add true or false
 AUTO_REPLY: process.env.AUTO_REPLY || "false", //add true or false
 ANTI_LINK: process.env.ANTI_LINK || "false", //add true or false
 ANTI_BAD: process.env.ANTI_BAD || "false", //add true or false
-FAKE_RECORDING: process.env.FAKE_RECORDING || "false", //add true or false
-AUTO_REACT: process.env.AUTO_REACT || "false", //add true or false
+FAKE_RECORDING: process.env.FAKE_RECORDING || "true", //add true or false
+AUTO_REACT: process.env.AUTO_REACT || "true", //add true or false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //add true or false
 ///=================When editing, do not accidentally delete anything. !!API-CONFIG=================================
 ///===============================================DONT-CHANGE-API=========================================
